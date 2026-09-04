bc72d024f2451c1b68bf1c3d92139b428d360c65
nijanthan46 <nijanthan20044002@gmail.com>
2026-09-04 11:30:33 +0530

    first commit

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
+       System.out.println("Hello World");
+       System.err.println("Welcome to Java");
     }
 }
