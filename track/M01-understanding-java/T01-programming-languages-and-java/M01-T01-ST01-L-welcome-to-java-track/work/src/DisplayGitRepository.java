eab2dbab69725a46c905f467ea1d623aa6e2e70c
nijanthan46 <nijanthan20044002@gmail.com>
2026-09-07 09:43:43 +0530

    Added git repository status program

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/DisplayGitRepository.java
@@ -0,0 +1,10 @@
+
+public class DisplayGitRepository {
+    public static void main(String[] args) {
+        // Display the Repository cloning status.
+        System.out.println("Repository: cloned successfully");
+
+        // Display whether the project is ready.
+        System.out.println("Project Status: Ready for Development");
+    }
+}
